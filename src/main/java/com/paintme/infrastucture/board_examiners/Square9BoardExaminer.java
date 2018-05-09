d8a649566070d9c5e0c64c00be7f55999b5e879f
NataliaSlobodianiuk <slo0nata@gmail.com>
2018-05-09 11:33:04 +0300

    Update square 9x9 board examiner

--- src/main/java/com/paintme/infrastucture/board_examiners/Square9BoardExaminer.java
+++ src/main/java/com/paintme/infrastucture/board_examiners/Square9BoardExaminer.java
@@ -1,70 +1,8 @@
 package com.paintme.infrastucture.board_examiners;
 
-import java.util.HashMap;
-import java.util.Map;
-
-public class Square9BoardExaminer extends SquareBoardExaminer {
+public class Square9BoardExaminer extends Square5BoardExaminer {
     public Square9BoardExaminer(){
         this.size = 9;
         this.winAmount = 4;
-    }
-
-    @Override
-    public char findWinningSymbol(String cells) {
-        char winningSymbol = '-';
-
-        Map<Integer[], Character> combinationsOfFourDictionary =
-                this.findCombinationOfFour(cells);
-        if (!combinationsOfFourDictionary.isEmpty()) {
-            winningSymbol = (char) combinationsOfFourDictionary
-                    .values().toArray()[0];
-        }
-
-        return winningSymbol;
-    }
-
-    @Override
-    protected Map<Integer[], Character> findCombinationOfTwoColumns(String cells) {
-        return new HashMap<>();
-    }
-
-    @Override
-    protected Map<Integer[], Character> findCombinationOfTwoDiagonals(String cells) {
-        return new HashMap<>();
-    }
-
-    @Override
-    protected Map<Integer[], Character> findCombinationOfTwoRows(String cells) {
-        return new HashMap<>();
-    }
-
-    @Override
-    protected Map<Integer[], Character> findCombinationOfThreeColumns(String cells) {
-        return new HashMap<>();
-    }
-
-    @Override
-    protected Map<Integer[], Character> findCombinationOfThreeDiagonals(String cells) {
-        return new HashMap<>();
-    }
-
-    @Override
-    protected Map<Integer[], Character> findCombinationOfThreeRows(String cells) {
-        return new HashMap<>();
-    }
-
-    @Override
-    protected Map<Integer[], Character> findCombinationOfFourColumns(String cells) {
-        return new HashMap<>();
-    }
-
-    @Override
-    protected Map<Integer[], Character> findCombinationOfFourDiagonals(String cells) {
-        return new HashMap<>();
-    }
-
-    @Override
-    protected Map<Integer[], Character> findCombinationOfFourRows(String cells) {
-        return new HashMap<>();
     }
 }
